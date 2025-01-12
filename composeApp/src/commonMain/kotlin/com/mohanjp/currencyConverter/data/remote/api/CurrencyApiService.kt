370bb75c6f66ac499ef96fe321b52f9e044f711d
“MohanJP” <jpmohan007@gmail.com>
2025-01-12 20:15:32 +0530

    initial commit

--- composeApp/src/commonMain/kotlin/com/mohanjp/currencyConverter/data/remote/api/CurrencyApiService.kt
+++ composeApp/src/commonMain/kotlin/com/mohanjp/currencyConverter/data/remote/api/CurrencyApiService.kt
@@ -2,6 +2,8 @@ package com.mohanjp.currencyConverter.data.remote.api
 
 class CurrencyApiService {
 
+    // todo implement api
+
     companion object {
         const val END_POINT = "https://api.currencyapi.com/v3/latest"
         const val API_KEY = "cur_live_HD8gMjuMZpczMQs4fOxvv2DqnmSXzCj5QkWkjjfV"
